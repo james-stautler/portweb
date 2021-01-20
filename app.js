5a78847d0448a2344b2c95613d0956fe5cb5e5d4
james-stautler <74434990+james-stautler@users.noreply.github.com>
2021-01-20 07:02:28 -0600

    cleaned up site, simplified pages and added fade in background

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const tl = gsap.timeline({ defaults: { ease: 'power1.out' } });
+const tl2 = gsap.timeline({ defaults: { ease: 'power1.out' } })
 
 tl.to('.title', { duration: 4, opacity: 1, stagger: { each: 1, from: 'start' } });
+tl2.to('.main', { duration: 4, opacity: 1 });
 tl.to('.arrow.down', { duration: 2, opacity: 1 }, '-=1.5');
